7f33234235202c5c2d8dc75b5d2db0d41a1af460
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-03-20 18:20:04 +0530

    minute changes

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <div className="h-28 w-full">
+            <div className="p-6 flex">
+                <h1 className="text-4xl font-semibold">Vindaddy</h1>
+            </div>
+        </div>
+    )
+}
